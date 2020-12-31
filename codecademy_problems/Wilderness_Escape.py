9270bc068622694019c059830a8f5ddb1b8bed94
Rubayet Mahmud <m.rubayet94@gmail.com>
2020-12-31 23:13:55 +0600

    adding chapters

--- codecademy_problems/Wilderness_Escape.py
+++ codecademy_problems/Wilderness_Escape.py
@@ -7,9 +7,10 @@ class TreeNode:
   def __init__(self, story_piece):
     self.story_piece = story_piece
     self.choices = []
-    
+
   def add_child(self, node):
     self.choices.append(node)
+
 
 ######
 # VARIABLES FOR TREE
@@ -23,11 +24,41 @@ story = \
     1 ) Roar back!
     2 ) Run to the left...
     """
-
 story_root = TreeNode(story)
+
+
+story_a = \
+"""
+The bear is startled and runs away.
+Do you:
+1 ) Shout 'Sorry bear!'
+2 ) Yell 'Hooray!'
+"""
+
+choice_a = TreeNode(story_a)
+
+
+story_b = \
+"""
+You come across a clearing full of flowers.
+The bear follows you and asks 'what gives?'
+Do you:
+1 ) Gasp 'A talking bear!'
+2 ) Explain that the bear scared you.
+"""
+
+choice_b = TreeNode(story_b)
+
+story_root.add_child(choice_a)
+story_root.add_child(choice_b)
+
+
+
+user_choice = input("What is your name? \n_> ")
+print(f"\nWelcome {user_choice}!")
 
 ######
 # TESTING AREA
 ######
 
-print(story_root.story_piece)
+print(story_root.choices[1].story_piece)
