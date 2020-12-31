9fb94b4651835bbb182fe373f3183ff1ada58411
Rubayet Mahmud <m.rubayet94@gmail.com>
2020-12-31 21:59:26 +0600

    remove child

--- codecademy_problems/tree.py
+++ codecademy_problems/tree.py
@@ -9,7 +9,18 @@ class TreeNode:
     print("Adding " + child_node.value)
     self.children.append(child_node)
 
+  def remove_child(self, child_node):
+    print(f"Removing {child_node.value} from {self.value}")
+    new_children = []
+    for node in self.children:
+      if node != child_node:
+        new_children.append(node)
+    self.children = new_children
+
 root = TreeNode("I am Root")
 child = TreeNode("A wee sappling")
+bad_seed = TreeNode("Root Rot!")
 
 root.add_child(child)
+root.add_child(bad_seed)
+root.remove_child(bad_seed)
