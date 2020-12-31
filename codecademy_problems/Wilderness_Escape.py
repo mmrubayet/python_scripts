44e1b24b0eec47bd32ac9a16738c5c39cb1f939e
Rubayet Mahmud <m.rubayet94@gmail.com>
2020-12-31 22:54:19 +0600

    Story Begins

--- codecademy_problems/Wilderness_Escape.py
+++ codecademy_problems/Wilderness_Escape.py
@@ -1,11 +1,30 @@
+print("Once upon a time . . .")
 ######
 # TREENODE CLASS
 ######
+
+class TreeNode:
+  def __init__(self, story_piece):
+    self.story_piece = story_piece
+    self.choices = []
 
 ######
 # VARIABLES FOR TREE
 ######
 
+story = \
+    """
+    You are in a forest clearing. There is a path to the left.
+    A bear emerges from the trees and roars!
+    Do you:
+    1 ) Roar back!
+    2 ) Run to the left...
+    """
+
+story_root = TreeNode(story)
+
 ######
 # TESTING AREA
 ######
+
+print(story_root.story_piece)
