2a7675f45c7017ed3c15705b2aeb7908ff05ae26
Rubayet Mahmud <m.rubayet94@gmail.com>
2020-12-31 21:58:01 +0600

    Think of the Children

--- codecademy_problems/tree.py
+++ codecademy_problems/tree.py
@@ -1,7 +1,15 @@
-# Defining "TreeNode" Python class 
+# Defining "TreeNode" Python class
 class TreeNode:
   def __init__(self, value):
     print("initializing node . . . ")
     self.value = value
+    self.children = []
 
-seed = TreeNode("RMM")
+  def add_child(self, child_node):
+    print("Adding " + child_node.value)
+    self.children.append(child_node)
+
+root = TreeNode("I am Root")
+child = TreeNode("A wee sappling")
+
+root.add_child(child)
