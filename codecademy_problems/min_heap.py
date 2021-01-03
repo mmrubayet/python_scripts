3dd1aa91a934be9dcb717f321533018438e89f22
Rubayet Mahmud <m.rubayet94@gmail.com>
2021-01-03 23:38:47 +0600

    Update min_heap.py

--- codecademy_problems/min_heap.py
+++ codecademy_problems/min_heap.py
@@ -2,3 +2,7 @@ class MinHeap:
   def __init__(self):
     self.heap_list = [None]
     self.count = 0
+
+  # define .add() below...
+  def add(self, element):
+    print(f"Adding {element} to {self.heap_list}")
