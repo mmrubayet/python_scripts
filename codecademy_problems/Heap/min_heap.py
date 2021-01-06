db9cbdcf50adc87fa5083f7fe2f9a8471356ab9c
Rubayet Mahmud <m.rubayet94@gmail.com>
2021-01-06 23:27:05 +0600

    adding helper methods

--- codecademy_problems/Heap/min_heap.py
+++ codecademy_problems/Heap/min_heap.py
@@ -3,6 +3,18 @@ class MinHeap:
     self.heap_list = [None]
     self.count = 0
 
+  # HEAP HELPER METHODS
+  def parent_idx(self, idx):
+    return idx // 2
+
+  def left_child_idx(self, idx):
+    return idx * 2
+
+  def right_child_idx(self, idx):
+    return idx * 2 + 1
+
+  # END OF HEAP HELPER METHODS
+
   def add(self, element):
     print(f"Adding {element} to {self.heap_list}")
     self.count += 1
@@ -10,4 +22,4 @@ class MinHeap:
     self.heapify_up()
 
   def heapify_up(self):
-    print("Restoring the heap property . . . ")
+    print("Heapifying up...")
