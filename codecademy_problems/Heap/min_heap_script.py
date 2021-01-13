dbd3d0aa1c2cd35c8bf74844237638197d69098b
Rubayet Mahmud <m.rubayet94@gmail.com>
2021-01-13 15:31:47 +0600

    Update min_heap_script.py

--- codecademy_problems/Heap/min_heap_script.py
+++ codecademy_problems/Heap/min_heap_script.py
@@ -1,4 +1,22 @@
-# importing heap class
+# import random number generator
+from random import randrange
+# import heap class
+from min_heap import MinHeap
+
+# make an instance of MinHeap
+min_heap = MinHeap()
+
+# populate min_heap with random numbers
+random_nums = [randrange(1, 101) for n in range(6)]
+for el in random_nums:
+  min_heap.add(el)
+
+
+# test it out, is the minimum number at index 1?
+print(min_heap.heap_list)
+
+
+'''# importing heap class
 from min_heap import MinHeap
 
 # an instance of MinHeap to use
@@ -33,3 +51,4 @@ print(idx_3_right_child_idx)
 print("The right child element of index 3 is:")
 
 print(min_heap.heap_list[idx_3_right_child_idx])
+'''
