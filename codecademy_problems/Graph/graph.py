a7d2cfe585f5d927154b50c5202195b7d80ad67e
Rubayet Mahmud <m.rubayet94@gmail.com>
2021-01-16 02:23:36 +0600

    Refactoring Path-Finding

--- codecademy_problems/Graph/graph.py
+++ codecademy_problems/Graph/graph.py
@@ -13,13 +13,20 @@ class Graph:
 
   def find_path(self, start_vertex, end_vertex):
     start = [start_vertex]
+    seen = {}
+
     while len(start) > 0:
       current_vertex = start.pop(0)
+      seen[current_vertex] = True
+
       print("Visiting " + current_vertex)
       if current_vertex == end_vertex:
         return True
+      else:
+        vertex = self.graph_dict[current_vertex]
+        next_vertices = vertex.get_edges()
 
-      vertex = self.graph_dict[current_vertex]
-      next_vertices = vertex.get_edges()
-      start += next_vertices
+        next_vertices = [vertex for vertex in next_vertices if vertex not in seen]
+        start.extend(next_vertices)
+
     return False
--- codecademy_problems/Graph/graph_script.py
+++ codecademy_problems/Graph/graph_script.py
@@ -1,33 +1,28 @@
 from graph import Graph
 from vertex import Vertex
 
-no_path_exists = True
+railway = Graph()
 
+callan = Vertex('callan')
+peel = Vertex('peel')
+ulfstead = Vertex('ulfstead')
+harwick = Vertex('harwick')
 
-directed_railway = Graph(True)
+railway.add_vertex(callan)
+railway.add_vertex(peel)
+railway.add_vertex(harwick)
+railway.add_vertex(ulfstead)
 
-callan_station = Vertex('callan')
-peel_station = Vertex('peel')
-ulfstead_station = Vertex('ulfstead')
-harwick_station = Vertex('harwick')
+railway.add_edge(peel, harwick)
+railway.add_edge(harwick, callan)
+railway.add_edge(callan, peel)
 
-directed_railway.add_vertex(callan_station)
-directed_railway.add_vertex(peel_station)
-directed_railway.add_vertex(harwick_station)
-directed_railway.add_vertex(ulfstead_station)
+# Uncomment the code below when you're done refactoring!
 
-directed_railway.add_edge(harwick_station, peel_station)
-directed_railway.add_edge(peel_station, callan_station)
+peel_to_ulfstead_path_exists = railway.find_path('peel', 'ulfstead')
+harwick_to_peel_path_exists = railway.find_path('harwick', 'peel')
 
-
-path_exists = directed_railway.find_path('harwick', 'harwick')
-print(path_exists)
-
-#Uncomment for final checkpoint
-
-print("\n\n\nFinding path from harwick to callan\n")
-new_path_exists = directed_railway.find_path('harwick', 'callan')
-print(new_path_exists)
-print("\n\nTrying to find path from harwick to ulfstead\n")
-no_path_exists = directed_railway.find_path('harwick', 'ulfstead')
-print(no_path_exists)
+print("A path exists between peel and ulfstead:")
+print(peel_to_ulfstead_path_exists)
+print("A path exists between harwick and peel:")
+print(harwick_to_peel_path_exists)
