530be260bd954f8768ea1ece92331fe1eb19f0f5
Rubayet Mahmud <m.rubayet94@gmail.com>
2021-01-15 22:25:20 +0600

    Update min_heap_script.py

--- codecademy_problems/Heap/min_heap_script.py
+++ codecademy_problems/Heap/min_heap_script.py
@@ -1,3 +1,33 @@
+# import random number generator
+from random import randrange
+# import heap class
+from min_heap import MinHeap
+
+# make an instance of MinHeap
+min_heap = MinHeap()
+
+# set internal list for testing purposes...
+min_heap.heap_list = [None, 10, 13, 21, 61, 22, 23, 99]
+min_heap.count = 7
+
+print("The smaller child of index 1 is: ")
+smaller_child_of_idx_1 = min_heap.get_smaller_child_idx(1)
+smaller_child_element = min_heap.heap_list[smaller_child_of_idx_1]
+print(smaller_child_element)
+
+print("The smaller child of index 2 is: ")
+smaller_child_of_idx_2 = min_heap.get_smaller_child_idx(2)
+smaller_child_element = min_heap.heap_list[smaller_child_of_idx_2]
+print(smaller_child_element)
+
+print("The smaller child of index 3 is: ")
+smaller_child_of_idx_3 = min_heap.get_smaller_child_idx(3)
+smaller_child_element = min_heap.heap_list[smaller_child_of_idx_3]
+print(smaller_child_element)
+
+
+'''
+EX: 8
 # import heap class
 from min_heap import MinHeap
 
@@ -11,9 +41,11 @@ min_heap.add(42)
 
 # remove minimum element
 print(min_heap.retrieve_min())
+'''
 
-
-'''# import random number generator
+'''
+EX: 7
+# import random number generator
 from random import randrange
 # import heap class
 from min_heap import MinHeap
