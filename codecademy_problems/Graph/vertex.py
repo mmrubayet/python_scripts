ed6adaceb15f28c2bba1a4fdaeb7ba588c237906
Rubayet Mahmud <m.rubayet94@gmail.com>
2021-01-16 00:29:12 +0600

    Building the Vertex II

--- codecademy_problems/Graph/vertex.py
+++ codecademy_problems/Graph/vertex.py
@@ -3,7 +3,19 @@ class Vertex:
     self.value = value
     self.edges = {}
 
+  def add_edge(self, vertex):
+    print(f"Adding edge to {vertex}")
+    self.edges[vertex] = True
+
   def get_edges(self):
     return list(self.edges.keys())
 
-station = Vertex("Cronk")
+
+grand_central = Vertex('Grand Central Station')
+forty_second_street = Vertex('42nd Street Station')
+
+print(grand_central.get_edges())
+
+grand_central.add_edge(forty_second_street.value)
+
+print(grand_central.get_edges())
