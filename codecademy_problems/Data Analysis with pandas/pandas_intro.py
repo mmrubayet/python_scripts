6d392bcf85f937ebc499eb01db3a028d6ba66a74
Rubayet Mahmud <m.rubayet94@gmail.com>
2021-01-17 15:11:59 +0600

    Creating a DataFrame II

--- codecademy_problems/Data Analysis with pandas/pandas_intro.py
+++ codecademy_problems/Data Analysis with pandas/pandas_intro.py
@@ -1,10 +1,15 @@
 import codecademylib
 import pandas as pd
 
-df1 = pd.DataFrame({
-  'Product ID': [1, 2, 3, 4],
-  'Product Name': ['t-shirt', 't-shirt', 'skirt', 'skirt'],
-  'Color': ['blue', 'green', 'red', 'black']
-})
+df2 = pd.DataFrame([
+  [1, 'San Diego', 100],
+  [2, 'Los Angeles', 120],
+  [3,	'San Francisco', 90],
+  [4, 'Sacramento', 115]
 
-print(df1)
+],
+  columns=[
+    'Store ID', 'Location', 'Number of Employees'
+  ])
+
+print(df2)
