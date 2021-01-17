d5c28e239b4e9f74afb558b1c6f0d5c348cc144a
Rubayet Mahmud <m.rubayet94@gmail.com>
2021-01-17 17:29:28 +0600

    Performing Column Operations

---
+++ codecademy_problems/Data Analysis with pandas/column_operations.py
@@ -0,0 +1,14 @@
+import codecademylib
+from string import lower
+import pandas as pd
+
+df = pd.DataFrame([
+  ['JOHN SMITH', 'john.smith@gmail.com'],
+  ['Jane Doe', 'jdoe@yahoo.com'],
+  ['joe schmo', 'joeschmo@hotmail.com']
+],
+columns=['Name', 'Email'])
+
+df['Lowercase Name'] = df.Name.apply(lower)
+
+print(df)
