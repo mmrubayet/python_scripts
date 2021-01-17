3e7682b8e601f46afd4cb6935899a7d725e921e5
Rubayet Mahmud <m.rubayet94@gmail.com>
2021-01-17 18:55:30 +0600

    Update renaming_columns.py

--- codecademy_problems/Data Analysis with pandas/renaming_columns.py
+++ codecademy_problems/Data Analysis with pandas/renaming_columns.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 df = pd.read_csv('imdb.csv')
 
-df.columns = ['ID', 'Title', 'Category', 'Year Released', 'Rating']
+# df.columns = ['ID', 'Title', 'Category', 'Year Released', 'Rating']
+df.rename(columns = {'name' : 'movie_title'}, inplace = True)
 
 print(df)
