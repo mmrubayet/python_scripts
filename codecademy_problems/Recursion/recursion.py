534f6b06da1e58200d791151b6502c3277599e95
Rubayet Mahmud <m.rubayet94@gmail.com>
2021-01-19 04:50:21 +0600

    Building Our Own Call Stack, Part II

--- codecademy_problems/Recursion/recursion.py
+++ codecademy_problems/Recursion/recursion.py
@@ -1,14 +1,19 @@
 def sum_to_one(n):
   result = 1
   call_stack = []
+
   while n != 1:
     execution_context = {"n_value": n}
     call_stack.append(execution_context)
     n -= 1
     print(call_stack)
   print("BASE CASE REACHED")
+
+  while call_stack:
+    return_value = call_stack.pop()
+    print(call_stack)
+    print(f"Return value of {return_value['n_value']} adding to result {result}")
+    result += return_value['n_value']
   return result, call_stack
-
-
 
 sum_to_one(4)
