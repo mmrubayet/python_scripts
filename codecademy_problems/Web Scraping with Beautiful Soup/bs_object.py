215299db8129f40ca36bbefc3259fa58a3aaf7db
Rubayet Mahmud <m.rubayet94@gmail.com>
2021-01-18 19:07:16 +0600

    Update bs_object.py

--- codecademy_problems/Web Scraping with Beautiful Soup/bs_object.py
+++ codecademy_problems/Web Scraping with Beautiful Soup/bs_object.py
@@ -6,4 +6,5 @@ webpage_response = requests.get('https://content.codecademy.com/courses/beautifu
 webpage = webpage_response.content
 soup = BeautifulSoup(webpage, "html.parser")
 
-print(soup.p.string)
+for parent in soup.div.parents:
+    print(parent)
