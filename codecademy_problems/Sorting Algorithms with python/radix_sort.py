a721f32c42476a2c5aee58211aef81e0666ab539
Rubayet Mahmud <m.rubayet94@gmail.com>
2021-01-19 19:07:43 +0600

    Update radix_sort.py

--- codecademy_problems/Sorting Algorithms with python/radix_sort.py
+++ codecademy_problems/Sorting Algorithms with python/radix_sort.py
@@ -1,4 +1,13 @@
 def radix_sort(to_be_sorted):
-  maximum_value = str(max(to_be_sorted))
-  max_exponent = len(maximum_value)
-  return max_exponent
+  maximum_value = max(to_be_sorted)
+  max_exponent = len(str(maximum_value))
+
+  being_sorted = to_be_sorted[:]
+  digits = [[] for i in range(10)]
+
+  for number in being_sorted:
+    number_as_a_string = str(number)
+    digit = number_as_a_string[-1]
+    digit = int(digit)
+
+    digits[digit].append(number)
