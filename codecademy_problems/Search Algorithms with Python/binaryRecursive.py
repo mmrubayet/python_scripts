88cf1dfc7d92a7e925042f67ad09367f8d45f8e6
Rubayet Mahmud <m.rubayet94@gmail.com>
2021-01-20 03:48:40 +0600

    Recursive Binary Search: Base Case

---
+++ codecademy_problems/Search Algorithms with Python/binaryRecursive.py
@@ -0,0 +1,11 @@
+def binary_search(sorted_list, target):
+  if not sorted_list:
+    return "value not found"
+  mid_idx = len(sorted_list) // 2
+  mid_val = sorted_list[mid_idx]
+  return mid_idx, mid_val
+
+# For testing:
+sorted_values = [13, 14, 15, 16, 17]
+print(binary_search([], 42))
+print(binary_search(sorted_values, 42))
