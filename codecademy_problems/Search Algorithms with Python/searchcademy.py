365a22ffb2e3a54c214a8a2a7b01f4e1888aad45
Rubayet Mahmud <m.rubayet94@gmail.com>
2021-01-20 04:38:01 +0600

    Create searchcademy.py

---
+++ codecademy_problems/Search Algorithms with Python/searchcademy.py
@@ -0,0 +1,17 @@
+def sparse_search(data, search_val):
+  print("Data: " + str(data))
+  print("Search Value: " + str(search_val))
+  first = 0
+  last = len(data) -1
+
+  while first <= last:
+    mid = (first + last)//2
+    if not data[mid]:
+      left = mid-1
+      right = mid+1
+
+      while(True):
+        if left < first and right > last:
+          print(f"{search_val} is not in the dataset")
+          return
+      elif right <= last and data[right]:
