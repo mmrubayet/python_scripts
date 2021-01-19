9b40a5593c2611189ce9336410551160509bf32d
Rubayet Mahmud <m.rubayet94@gmail.com>
2021-01-19 16:42:23 +0600

    Update merge_sort.py

--- codecademy_problems/Sorting Algorithms with python/merge_sort.py
+++ codecademy_problems/Sorting Algorithms with python/merge_sort.py
@@ -9,11 +9,19 @@ def merge_sort(items):
   return middle_index, left_split, right_split
 
 def merge(left, right):
-  while (left and right):
-    if left[0] < right[0]:
-      result.append(left.pop(0))
-    else:
-      result.append(right.pop(0))
   result = []
 
+  while (left and right):
+    if left[0] < right[0]:
+      result.append(left[0])
+      left.pop(0)
+    else:
+      result.append(right[0])
+      right.pop(0)
+
+  if left:
+    result += left
+  if right:
+    result += right
+    
   return result
