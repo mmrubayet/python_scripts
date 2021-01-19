64fedbd91492ddf0ca32c485334f24373932d63d
Rubayet Mahmud <m.rubayet94@gmail.com>
2021-01-19 17:17:10 +0600

    Create quicksort.py

---
+++ codecademy_problems/Sorting Algorithms with python/quicksort.py
@@ -0,0 +1,11 @@
+def quicksort(list, start, end):
+  if start >= end:
+    return
+  print(list[start])
+  start += 1
+  quicksort(list, start, end)
+
+
+colors = ["blue", "red", "green", "purple", "orange"]
+
+quicksort(colors, 0, 5)
