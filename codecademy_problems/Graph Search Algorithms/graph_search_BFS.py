4951f94e3c82bc24faf2a41df7c9ef4c6e26ec0b
Rubayet Mahmud <m.rubayet94@gmail.com>
2021-01-20 05:30:39 +0600

    Update graph_search_BFS.py

--- codecademy_problems/Graph Search Algorithms/graph_search_BFS.py
+++ codecademy_problems/Graph Search Algorithms/graph_search_BFS.py
@@ -1,5 +1,5 @@
 def bfs(graph, start_vertex, target_value):
   path = [start_vertex]
-  return path
-
-print(bfs(None, "lava!", None))
+  vertex_and_path = [start_vertex, path]
+  bfs_queue = [vertex_and_path]
+  visited = set()
