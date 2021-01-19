3bdfe22b950ba6797a722cfb58996ce9b6d3fcee
Rubayet Mahmud <m.rubayet94@gmail.com>
2021-01-20 05:19:06 +0600

    Update graph_search.py

--- codecademy_problems/Graph Search Algorithms/graph_search.py
+++ codecademy_problems/Graph Search Algorithms/graph_search.py
@@ -1,5 +1,6 @@
-def dfs(graph, current_vertex, target_value, visited = None):
+def dfs(graph, current_vertex, target_value, visited=None):
   if visited == None:
     visited = []
-    visited.append(current_vertex)
+  visited.append(current_vertex)
+  if current_vertex == target_value:
     return visited
